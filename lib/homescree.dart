547badb2c75a5ed8f626d02381e59caedc4d5135
itsrabii <142924291+rabiaryal@users.noreply.github.com>
2024-12-05 19:51:41 +0545

    fist animation done

--- lib/homescree.dart
+++ lib/homescree.dart
@@ -1,31 +1,63 @@
 import 'package:flutter/material.dart';
 import 'dart:math' show pi;
 
-class HomePageScreen extends StatefulWidget {
-  const HomePageScreen({super.key});
+class RotatingRectAnimatiosn extends StatefulWidget {
+  const RotatingRectAnimatiosn({super.key});
 
   @override
-  State<HomePageScreen> createState() => _HomePageScreenState();
+  State<RotatingRectAnimatiosn> createState() => _RotatingRectAnimatiosnState();
 }
 
-class _HomePageScreenState extends State<HomePageScreen>
+class _RotatingRectAnimatiosnState extends State<RotatingRectAnimatiosn>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _animation;
 
   @override
-  void initstate() {
+  void initState() {
     super.initState();
     _controller =
         AnimationController(vsync: this, duration: const Duration(seconds: 2));
 
     _animation = Tween<double>(begin: 0.0, end: 2 * pi).animate(_controller);
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Animations"),
+      ),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.identity()..rotateY(_animation.value),
+              child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3))
+                    ]),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
