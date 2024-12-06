45892cdbbeb4115ee43df1f41e545e0e7496e0a8
itsrabii <142924291+rabiaryal@users.noreply.github.com>
2024-12-07 00:33:21 +0545

    hero animation doen

--- lib/otheranimation/heroanimation.dart
+++ lib/otheranimation/heroanimation.dart
@@ -32,6 +32,7 @@ class _HeroAnimationState extends State<HeroAnimation> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("People"),
+        backgroundColor: Colors.black.withRed(50),
         centerTitle: true,
       ),
       body: ListView.builder(
@@ -41,12 +42,83 @@ class _HeroAnimationState extends State<HeroAnimation> {
             return ListTile(
               title: Text(person.name),
               subtitle: Text('${person.age} years Old '),
-              leading: Text(
-                person.emoji,
-                style: const TextStyle(fontSize: 40),
+              leading: Hero(
+                tag: person.emoji,
+                child: Text(
+                  person.emoji,
+                  style: const TextStyle(fontSize: 40),
+                ),
               ),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => DetailPage(person: person)));
+              },
             );
           }),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  final Person person;
+
+  const DetailPage({super.key, required this.person});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black.withRed(10),
+        title: Hero(
+          flightShuttleBuilder: (flightContext, animation, flightDirection,
+              fromHeroContext, toHeroContext) {
+            switch (flightDirection) {
+              case HeroFlightDirection.push:
+                return Material(
+                    color: Colors.transparent,
+                    child: ScaleTransition(
+                        scale: animation.drive(Tween<double>(
+                          begin: 0.0,
+                          end: 1.0,
+                        ).chain(CurveTween(curve: Curves.fastOutSlowIn))),
+                        child: toHeroContext.widget));
+              case HeroFlightDirection.pop:
+                return Material(
+                    color: Colors.transparent, child: fromHeroContext.widget);
+            }
+          },
+          tag: person.emoji,
+          child: Text(
+            person.emoji,
+            style: const TextStyle(fontSize: 40),
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 15,
+            ),
+            Hero(
+              tag: person.name,
+              child: Text(
+                person.name,
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              '${person.age}',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
