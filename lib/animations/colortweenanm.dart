cfc5ebd1ab494538a0b0475f5de99d1364f8cee5
itsrabii <142924291+rabiaryal@users.noreply.github.com>
2024-12-07 15:36:47 +0545

    finally it works

--- lib/animations/colortweenanm.dart
+++ lib/animations/colortweenanm.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
 
 class TweenColorAnm extends StatefulWidget {
   const TweenColorAnm({super.key});
@@ -16,10 +22,7 @@ class CircleClipper extends CustomClipper<Path> {
         center: Offset(size.width / 2, size.height / 2),
         radius: size.width / 2);
 
-    path.addOval(Rect.fromCircle(
-      center: Offset(size.width / 2, size.height / 2),
-      radius: size.width / 2,
-    ));
+    path.addOval(rect);
 
     return path;
   }
@@ -28,16 +31,47 @@ class CircleClipper extends CustomClipper<Path> {
   bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 }
 
+Color getRandomColor() => Color(
+      0xFF000000 +
+          math.Random().nextInt(
+            0x00FFFFFF,
+          ),
+    );
+
 class _TweenColorAnmState extends State<TweenColorAnm> {
+  var _color = getRandomColor();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ClipPath(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.width,
-            color: Colors.red,
+          clipper: CircleClipper(),
+          child: TweenAnimationBuilder(
+            tween: ColorTween(
+              begin: getRandomColor(),
+              end: _color,
+            ),
+            onEnd: () {
+              setState(() {
+                _color = getRandomColor();
+              });
+            },
+            duration: const Duration(seconds: 1),
+            builder: (context, Color? color, child) {
+              'Color Changed'.log();
+              return ColorFiltered(
+                colorFilter: ColorFilter.mode(
+                  color!,
+                  BlendMode.srcATop,
+                ),
+                child: child,
+              );
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              color: Colors.red,
+            ),
           ),
         ),
       ),
